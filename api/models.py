14aa54787c26d0cc0e04e3d7a28b6382b5f7496f
ibinkows <igor.binkowski@nokia.com>
2021-03-27 20:32:15 +0100

    added serializer logic

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Dish(models.Model):
-    name = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=100, null=False, unique=True)
     description = models.CharField(max_length=1000)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=False)
     preparation_time = models.DurationField(null=False)
--- api/serializers.py
+++ api/serializers.py
@@ -4,12 +4,58 @@ from .models import Dish, Menu
 
 
 class MenuListSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Menu
+        fields = [
+            'name',
+            'description',
+            'dish_count',
+            ]
+
+    dish_count = serializers.SerializerMethodField(read_only=True)
+
+    def get_dish_count(self, obj):
+        return obj.dishes.count()
+    
+
+class DishDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dish
+        fields = [
+            'name',
+            'description',
+            'price',
+            'preparation_time',
+            'is_vegetarian',
+            'date_added',
+            'date_updated',
+            ]
 
 
 class MenuDetailSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Menu
+        fields = [
+            'name',
+            'description',
+            'date_added',
+            'date_updated',
+            'dishes',
+            ]
+        
+    dishes = DishDetailSerializer(many=True)
 
 
 class MenuCreateSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Menu
+        fields = [
+            'name',
+            'description',
+            'dishes',
+        ]
+
+    dishes = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=Dish.objects.all())
