8bff9f39d82f76360b2372c76d04e9fb509ea4bc
ibinkows <igor.binkowski@nokia.com>
2021-03-27 19:41:38 +0100

    added view logic

--- api/views.py
+++ api/views.py
@@ -7,18 +7,45 @@ from .serializers import MenuListSerializer, MenuDetailSerializer, MenuCreateSer
 
 
 @api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticatedOrReadOnly])
+# @permission_classes([IsAuthenticatedOrReadOnly])
 def menu_list(request):
     """
     View all menus with GET, or add a new one with POST.
     """
-    return Response()
+    if request.method == 'GET':
+        # TODO: add filtering options
+        qs = Menu.objects.all()
+        serializer = MenuListSerializer(qs, many=True)
+        return Response(serializer.data, status=200)
+    
+    if request.method == 'POST':
+        serializer = MenuCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response({}, status=400)
 
 
 @api_view(['GET', 'PATCH'])
-@permission_classes([IsAuthenticatedOrReadOnly])
-def menu_detail(request):
+# @permission_classes([IsAuthenticatedOrReadOnly])
+def menu_detail(request, menu_id):
     """
     View an existing menu with GET, or update it with PATCH.
     """
-    return Response()
+    try:
+        menu = Menu.objects.get(id=menu_id)
+    except Menu.DoesNotExist:
+        return Response({}, status=404) 
+
+    if request.method == 'GET':
+        serializer = MenuDetailSerializer(menu)
+        return Response(serializer.data, status=200)
+
+    if request.method == 'PATCH':
+        serializer = MenuCreateSerializer(menu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        else:
+            return Response({}, status=400)
