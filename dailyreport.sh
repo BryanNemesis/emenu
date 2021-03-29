564712fd77c12c6d43b50a90896770ade1160236
ibinkows <igor.binkowski@nokia.com>
2021-03-29 20:48:25 +0200

    changed location of dailyreport log

--- dailyreport.sh
+++ dailyreport.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-touch /tmp/dailyreport.log
-echo "0 10 * * * python /home/app/manage.py dailyreport >> /tmp/dailyreport.log 2>&1" > /etc/crontabs/root
+touch /home/dailyreport.log
+echo "0 10 * * * python /home/app/manage.py dailyreport >> /home/dailyreport.log 2>&1" > /etc/crontabs/root
 crond start
-tail -f /tmp/dailyreport.log
+tail -f /home/dailyreport.log
